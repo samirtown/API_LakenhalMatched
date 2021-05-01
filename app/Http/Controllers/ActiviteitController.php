fa6bf06efd10bb60c987d2de51e42013ee54ca54
samirtown <samirf4thi@outlook.com>
2021-05-01 15:46:16 +0200

    Activiteit Controller

--- app/Http/Controllers/ActiviteitController.php
+++ app/Http/Controllers/ActiviteitController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Activiteit;
 
 class ActiviteitController extends Controller
-{
-    /*
+{ 
     public function show($activiteit_ID){
         return Activiteit::where('activiteit_ID','=',$activiteit_ID)->first();
-      }
+    }
     
       public function index(){
         return Activiteit::all();
-      } */
+    } 
 }
