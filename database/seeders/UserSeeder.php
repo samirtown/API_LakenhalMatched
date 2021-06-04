da0000019c663934a5bab13d3f95175473e9a239
samirtown <samirf4thi@outlook.com>
2021-06-04 15:36:33 +0200

    user admin & user seeds

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,23 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            
+            'naam' => 'Yasmine',
+            'email' => 'lakenhalmoderator@gmail.com',
+            'password' => bcrypt('Lakens1101813nettav.'),
+            'beroep' => 'moderator',
+            'admin' => true
+        ]);
+        DB::table('users')->insert([
+            'naam' => 'Ilse storm',
+            'email' => 'ilsestorm@gmail.com',
+            'password' => bcrypt('Lakens1101813nettav.'),
+            'beroep' => 'Filmmaker',
+        ]);
+        DB::table('users')->insert([
+            'naam' => 'Henk Krol',
+            'email' => 'henkkrol@gmail.com',
+            'password' => bcrypt('Lakens1101813nettav.'),
+            'beroep' => 'Informaticus',
         ]);
     }
 }
