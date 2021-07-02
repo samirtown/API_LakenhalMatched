2510030c99084ad932e14bb1cc7e2167d9b91971
thimohenraat <thimohenraat@gmail.com>
2021-07-03 00:20:19 +0200

    user groep joins goed gemaakt

--- app/Http/Controllers/UserGroepschatController.php
+++ app/Http/Controllers/UserGroepschatController.php
@@ -24,7 +24,9 @@ class UserGroepschatController extends Controller
 
     //Zie welke users er allemaal in een groepschat zitten met de activiteit informatie
     public function usersInGroepActiviteit($user_ID){
-        return UserGroepschat::where('user_ID','=',$user_ID)->join('groepschat', 'user_groepschat.groepschat_ID', '=', 'groepschat.groepschat_ID')
-        ->join('activiteit', 'groepschat.groepschat_ID', '=', 'activiteit.activiteit_ID')->get();
+        return UserGroepschat::where('user_groepschat.user_ID','=',$user_ID)
+        ->join('groepschat', 'user_groepschat.groepschat_ID', '=', 'groepschat.groepschat_ID')
+        ->join('activiteit', 'groepschat.groepschat_ID', '=', 'activiteit.activiteit_ID')
+        ->get();
     }
 }
