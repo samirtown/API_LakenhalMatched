73055377b06201c8ebd1341e3238626b7c6c1d3a
samirtown <samirf4thi@outlook.com>
2021-07-05 16:25:33 +0200

    profile zie posts in fix

--- app/Http/Controllers/ActiviteitController.php
+++ app/Http/Controllers/ActiviteitController.php
@@ -23,7 +23,7 @@ class ActiviteitController extends Controller
     }
 
     public function activiteitenUsersProfiel($user_ID){
-        return Activiteit::join('users', 'activiteit.user_ID', '=', 'users.user_ID')->get()->where('user_ID','=',$user_ID);
+        return Activiteit::join('users', 'activiteit.user_ID', '=', 'users.user_ID')->where('users.user_ID','=',$user_ID)->get();
     }
 
     public function activiteitenGerapporteerd(){
